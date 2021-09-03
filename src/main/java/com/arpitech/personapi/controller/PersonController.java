3953e48733bda110232f517969015935e4607f46
sandroteste72 <sandro.teste72@gmail.com>
2021-09-03 02:09:32 -0300

    Add controller

--- src/main/java/com/arpitech/personapi/controller/PersonController.java
+++ src/main/java/com/arpitech/personapi/controller/PersonController.java
@@ -7,4 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
+    public String getString() {
+        return "API Person DIO";
+    }
 }
