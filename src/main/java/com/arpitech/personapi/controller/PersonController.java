6c4264a0b42b8f4251c9b4f94c7939f5e3a5c9ab
sandroteste72 <sandro.teste72@gmail.com>
2021-09-03 14:11:09 -0300

    Created README.md file

--- src/main/java/com/arpitech/personapi/controller/PersonController.java
+++ src/main/java/com/arpitech/personapi/controller/PersonController.java
@@ -4,6 +4,7 @@ import com.arpitech.personapi.dto.request.PersonDTO;
 import com.arpitech.personapi.dto.response.MessageResponseDTO;
 import com.arpitech.personapi.exception.PersonNotFoundException;
 import com.arpitech.personapi.service.PersonService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,14 +14,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
     private PersonService personService;
-
-    @Autowired
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
